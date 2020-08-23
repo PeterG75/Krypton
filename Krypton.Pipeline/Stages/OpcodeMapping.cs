116529ead58bfa6defdd45624e58876b0d573851
TobitoFatito <7544428+TobitoFatitoNulled@users.noreply.github.com>
2020-08-23 19:10:49 +0300

    Opcode Mapping

---
+++ Krypton.Pipeline/Stages/OpcodeMapping.cs
@@ -0,0 +1,12 @@
+﻿using Krypton.Core;
+
+namespace Krypton.Pipeline.Stages
+{
+    public class OpcodeMapping : IStage
+    {
+        public string Name => nameof(OpcodeMapping);
+        public void Run(DevirtualizationCtx Ctx)
+        {
+        }
+    }
+}
